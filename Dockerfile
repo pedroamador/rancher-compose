ee152b47712841f9db679916c548e9b2822a700a
madoos <maurice.ronet.dominguez@gmail.com>
2018-01-16 01:29:18 +0100

    New: Implement entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,19 @@
-FROM  redpandaci/ubuntu-dind:latest
+FROM  alpine:latest
 
 LABEL Mantainer Maurice Domínguez <maurice.ronet.dominguez@gmail.com>
 
 WORKDIR /workspace
 
+ENV RANCHER_COMPOSE_VERSION 0.12.5
+RUN wget https://github.com/rancher/rancher-compose/releases/download/v$RANCHER_COMPOSE_VERSION/rancher-compose-linux-amd64-v$RANCHER_COMPOSE_VERSION.tar.gz && \
+    tar zxf rancher-compose-linux-amd64-v$RANCHER_COMPOSE_VERSION.tar.gz && \
+    mv rancher-compose-v$RANCHER_COMPOSE_VERSION/rancher-compose /usr/local/bin/rancher-compose && \
+    chmod +x /usr/local/bin/rancher-compose && \
+    rm rancher-compose-linux-amd64-v$RANCHER_COMPOSE_VERSION.tar.gz && \
+    rm -r rancher-compose-v$RANCHER_COMPOSE_VERSION
+
 COPY entrypoint.sh /usr/bin/entrypoint.sh
 
 RUN chmod u+x /usr/bin/entrypoint.sh
 
-ENTRYPOINT ["/usr/bin/entrypoint.sh"]
+CMD ["/usr/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,59 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo "ENTRYPOINT"
+set -xe
+
+rancher_exec(){
+  local cmd host project key secret
+  cmd=$1
+  host=$2
+  project=$3
+  key=$4
+  secret=$5
+
+  rancher-compose --project-name $project --url $host --access-key $key --secret-key $secret --verbose $cmd
+}
+
+deploy(){
+  local host project key secret
+  host=$1
+  project=$2
+  key=$3
+  secret=$4
+
+  rancher_exec "create" $host $project $key $secret
+  rancher_exec "start" $host $project $key $secret
+}
+
+update_deployment(){
+  local host project key secret
+  host=$1
+  project=$2
+  key=$3
+  secret=$4
+
+  rancher-compose --project-name $project \
+  --url $host \
+  --access-key $key \
+  --secret-key $secret \
+  up -d --force-upgrade --pull --confirm-upgrade
+}
+
+write_docker_compose_from_pipe(){
+  local IFS; IFS=''
+  
+  while read data; do
+    echo "$data" >> docker-compose.yml
+  done
+}
+
+main() {
+    write_docker_compose_from_pipe;
+
+    if [ "$EXEC" = "create" ]; then
+        deploy $HOST $PROJECT $KEY $SECRET;
+    elif [ "$EXEC" = "update" ]; then
+        update_deployment $HOST $PROJECT $KEY $SECRET;
+    fi
+}
+
+main
