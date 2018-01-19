e8b9b4c8371ef63002ff17120ddc4e1a9e16cbb2
madoos <maurice.ronet.dominguez@gmail.com>
2018-01-19 12:06:31 +0100

    Build: Adds install.sh script

---
+++ util/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+download_cli(){
+    local url
+    url=$1
+    path=$2
+    curl -o $path $url
+}
+
+install(){
+    local url path version
+    version="develop"
+    path="/usr/local/bin/redpanda-rancher"
+    url="https://raw.githubusercontent.com/red-panda-ci/rancher-compose/$version/util/cli.sh"
+
+    echo "Instaling redpanda-rancher"
+    download_cli $url $path
+    chmod +x $path
+    echo "done"
+}
+
+
+install
