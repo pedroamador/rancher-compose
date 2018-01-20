28276a85d575f209672dc76f65a9bb2e2aeb6f99
madoos <maurice.ronet.dominguez@gmail.com>
2018-01-20 11:29:21 +0100

    Update: Set sudo privileges to install.sh script

--- util/install.sh
+++ util/install.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-
 download_cli(){
     local url
     url=$1
     path=$2
-    curl -o $path $url
+    sudo curl -o $path $url
 }
 
 install(){
     local url path version
-    version="develop"
+    version="1.0.0"
     path="/usr/local/bin/redpanda-rancher"
     url="https://raw.githubusercontent.com/red-panda-ci/rancher-compose/$version/util/cli.sh"
 
     echo "Instaling redpanda-rancher"
     download_cli $url $path;
-    chmod +x $path
+    sudo chmod +x $path
     echo "done"
 }
-
 
 install
